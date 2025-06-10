33b083d2ad60b13853198688ad984e89708e39f5
kksun0202 <kksun0202@naver.com>
2025-06-10 17:44:08 +0900

    20250610 useState / input

---
+++ react-study/src/study/components/State/InputState1/InputState1.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function InputState1() {
+    const [ inputValue, setInputValue ] = useState("");
+    const [ h1Text, seth1Text ] = useState("");
+    const [ inputValue2, setInputValue2 ] = useState("");
+    const [ h1Text2, seth1Text2 ] = useState("");
+
+    console.log("렌더링");
+
+    const handleOnChange = (e) => {
+        setInputValue(e.target.value)
+    }
+    const handleOnChange2 = (e) => {
+        setInputValue2(e.target.value)
+    }
+
+    const handleOnClick = (e) => {
+        seth1Text(inputValue);
+        seth1Text2(inputValue2);
+
+    }
+
+    return <div>
+        <h1>{h1Text}</h1>
+        <h1>{h1Text2}</h1>
+        <input className="input" type="text" value={inputValue} onChange={handleOnChange} />
+        <input className="input" type="text" value={inputValue2} onChange={handleOnChange2} />
+        <button onClick={handleOnClick}>확인</button>
+    </div>
+}
+
+export default InputState1;
